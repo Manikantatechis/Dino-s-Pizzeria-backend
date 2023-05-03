9fde3fbbcd1529b938f550c56b16a0bcb2b767ca
Manikantatechis <97092725+Manikantatechis@users.noreply.github.com>
2023-05-03 23:00:36 +0530

    Add files via upload

---
+++ apps/carts/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework.generics import ListCreateAPIView
+from .models import Cart
+from rest_framework import serializers
+
+
+class CartSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Cart
+        fields = ["id", "quantity", "item"]
+        depth = 1
+
+
+class CartAddSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+class CartUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
